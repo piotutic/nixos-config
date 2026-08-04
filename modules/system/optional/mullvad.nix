e14111df08726e62ea997ab1d37baa7a2f1df23a
piotutic <piotutic@yahoo.com>
2026-08-04 04:03:50 -0400

    config: 2026-08-04 04:03:50

--- modules/system/optional/mullvad.nix
+++ modules/system/optional/mullvad.nix
@@ -1,18 +1,11 @@
-{ pkgs, ... }:
+{ ... }:
 
 {
   services.mullvad-vpn.enable = true;
-  services.mullvad-vpn.package = pkgs.mullvad-vpn;
-
-  environment.systemPackages = with pkgs; [
-    mullvad-vpn
-  ];
+  # `pkgs.mullvad-vpn` is now GUI-only; the daemon lives in `pkgs.mullvad`,
+  # which is the default for `services.mullvad-vpn.package`.
+  services.mullvad-vpn.gui.enable = true;
 
   networking.wireguard.enable = true;
   networking.firewall.enable = true;
-
-  systemd.services.mullvad-daemon = {
-    enable = true;
-    wantedBy = [ "multi-user.target" ];
-  };
 }
