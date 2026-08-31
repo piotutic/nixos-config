17bcd2a2b45773c9bb35510aa5628bdc716ebb2d
piotutic <piotutic@yahoo.com>
2026-08-31 01:41:34 -0400

    config: 2026-08-31 01:41:34

--- flake.nix
+++ flake.nix
@@ -9,10 +9,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    llm-agents = {
-      url = "github:numtide/llm-agents.nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    llm-agents.url = "github:numtide/llm-agents.nix";
 
   };
 
--- modules/system/optional/gui/session.nix
+++ modules/system/optional/gui/session.nix
@@ -15,7 +15,6 @@
     epiphany
     geary
     gnome-music
-    gnome-photos
     simple-scan
     yelp
   ];
